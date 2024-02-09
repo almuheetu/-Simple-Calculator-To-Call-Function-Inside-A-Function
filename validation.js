8a21167e21f3283a3a5b6a9ffb47264b62c58348
Almuheetu Shihab <almuheetu@gmail.com>
2024-02-09 17:01:27 +0600

    practice js codeing

--- validation.js
+++ validation.js
@@ -1,5 +1,5 @@
 function multiply(num1, num2) {
-    if(typeof num1 !== 'number'){
+    if(typeof num1 !== 'number' || typeof num2 !== 'number'){
         return 'please privide a number'
     }
     const mult = num1 * num2;
@@ -8,4 +8,13 @@ function multiply(num1, num2) {
 }
 
 const result = multiply(5, 5 );
-console.log(result);
+// console.log(result);
+
+
+// for practice
+function fullName (first, second){
+    const full = first + ' ' + second;
+    return full;
+}
+const full = fullName ('Almuheetu', 'shihab')
+console.log(full);
